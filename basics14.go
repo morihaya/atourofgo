2ad48d341b75eae108c569f14f0b68070cee3039
morihaya <yukiya.hayashi@gmail.com>
2022-01-01 22:07:14 +0900

    finished basics 14

---
+++ basics14.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := 42
+	fmt.Printf("v is of type %T\n", v)
+	w := 42.2
+	fmt.Printf("w is of type %T\n", w)
+	x := "42.2"
+	fmt.Printf("x is of type %T\n", x)
+}
